cc8fa60035a2b4c9e20d458eae70e66f135e9ace
Alex Brezas <abresas@gmail.com>
2024-02-13 11:12:54 +0200

    Revert "add message_provider option on multisig handler"
    This reverts commit 213231436bf41fa13bc7444d60fbf1f663ba84a0.

--- ampd/src/handlers/config.rs
+++ ampd/src/handlers/config.rs
@@ -33,7 +33,6 @@ pub enum Config {
     },
     MultisigSigner {
         cosmwasm_contract: TMAddress,
-        message_provider: Option<TMAddress>,
     },
     SuiMsgVerifier {
         cosmwasm_contract: TMAddress,
